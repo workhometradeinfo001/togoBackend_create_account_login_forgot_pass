58f105e3506eaae719981abfaccb4759384ba7ad
workhometradeinfo001 <work.home.trade.info.001@gmail.com>
2026-07-15 01:01:17 +0600

    Successful connect with oracle cloud Mysql Heatwave DB

---
+++ src/main/java/com/togo/login_rf/LoginRfApplication.java
@@ -0,0 +1,13 @@
+package com.togo.login_rf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/togo/login_rf/LoginRfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.togo.login_rf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
