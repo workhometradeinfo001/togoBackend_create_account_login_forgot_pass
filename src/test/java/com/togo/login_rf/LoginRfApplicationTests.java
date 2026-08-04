084e0d65577452fd86cbd935617888976fd1bd54
workhometradeinfo001 <work.home.trade.info.001@gmail.com>
2026-08-04 09:30:25 +0600

    Added profile activation

--- src/test/java/com/togo/login_rf/LoginRfApplicationTests.java
+++ src/test/java/com/togo/login_rf/LoginRfApplicationTests.java
@@ -2,8 +2,10 @@ package com.togo.login_rf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LoginRfApplicationTests {
 
 	@Test
