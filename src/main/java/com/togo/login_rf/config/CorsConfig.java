14cea47df10fa30eb8f364683eccc5820ed689ac
workhometradeinfo001 <work.home.trade.info.001@gmail.com>
2026-08-19 10:13:27 +0600

    solve minur issue

--- src/main/java/com/togo/login_rf/config/CorsConfig.java
+++ src/main/java/com/togo/login_rf/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.togo.login_rf.config;
 
 import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,6 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration(proxyBeanMethods = false)
 public class CorsConfig {
+
+    @Value("${app.value.link}")
+    private String frontEndOrigin;
 
     @Bean
     public WebMvcConfigurer corsConfigure(){
@@ -18,7 +22,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         //Frontend side
                         .allowedOrigins("http://localhost:5173")
-                        .allowedOrigins("https://fron-end-react-container.ambitiousmoss-49f2f168.southeastasia.azurecontainerapps.io")
+                        .allowedOrigins(frontEndOrigin)
                         //Api Call
                         .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
                         //Auth of JWT
